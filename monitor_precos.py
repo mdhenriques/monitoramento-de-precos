b82c9706f2b4513f81914b3b7a68fc754b70f00d
Murilo Duarte <md.henriques@outlook.com>
2024-02-15 23:48:35 -0300

    Extraindo nome e valor com centavos

--- monitor_precos.py
+++ monitor_precos.py
@@ -1,5 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+
+def criar_dataframe(resultado):
+    df = pd.DataFrame(resultado)
+    return df
+
+def salvar_dataframe_excel(df, nome_arquivo):
+    df.to_excel(nome_arquivo + '.xlsx', index=False, engine='xlsxwriter')
+    print(f'DataFrame salvo como {nome_arquivo}')
 
 def extrair_dados_produto(url):
 
@@ -12,8 +21,19 @@ def extrair_dados_produto(url):
         nome_produto = item.find('h2', class_='ui-search-item__title').text.strip()
         
         preco_produto = item.find('span', class_='andes-money-amount__fraction').text.strip()
-        produtos.append({'nome':nome_produto, 'preco': preco_produto})
+
+        centavos_existe = item.find('span', class_='andes-money-amount__cents andes-money-amount__cents--superscript-24')
+        centavos = centavos_existe.text.strip() if centavos_existe else '00'
+
+        produtos.append({'nome':nome_produto, 'preco': preco_produto + ',' + centavos})
+
     return produtos
+
+
 resultado = extrair_dados_produto("https://lista.mercadolivre.com.br/fone-de-ouvido_ITEM*CONDITION_2230284_NoIndex_True")
 
-print(len(resultado))
+df = criar_dataframe(resultado)
+
+salvar_dataframe_excel(df, 'tabela-produtos')
+
+
