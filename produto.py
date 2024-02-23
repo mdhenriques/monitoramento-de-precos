2ad47b1ed4b3aad4dcf45e3fa9b816d1e3e31622
Murilo Duarte <md.henriques@outlook.com>
2024-02-23 15:09:28 -0300

    Comentando campos de avaliacao

--- produto.py
+++ produto.py
@@ -2,10 +2,9 @@ class Produto:
     def __init__(self, nome, preco, avaliacao_media, num_avaliacoes):
         self.nome = nome
         self.preco = preco
-        self.avaliacao_media = avaliacao_media
-        self.num_avaliacoes = num_avaliacoes
+#        self.avaliacao_media = avaliacao_media
+#        self.num_avaliacoes = num_avaliacoes
 
-    def __str__(self):
-        return f"Produto: {self.nome}\nPreço: {self.preco}\nAvaliação Média: {self.avaliacao_media}\nNúmero de Avaliações: {self.num_avaliacoes}"
+    
 
 
