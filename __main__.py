c625b0f7063421fb7423c9c8f5437cd57f4403bf
Murilo Henriques <md.henriques@outlook.com>
2024-03-02 18:04:16 -0300

    Removendo funcoes desnecessarias, adicionando armazenamento em json

--- __main__.py
+++ __main__.py
@@ -1,6 +1,5 @@
 from utils.extracao import  extrair_dados_varias_paginas, extrair_dados_produto
-from utils.manipulacao import criar_dataframe, salvar_dataframe_excel
-
+from utils.manipulacao import criar_dataframe
 def main():
     base_url = "https://lista.mercadolivre.com.br/fone-de-ouvido_ITEM*CONDITION_2230284_NoIndex_True"
 
@@ -14,8 +13,8 @@ def main():
     filtro = "Fone De Ouvido"
     df = criar_dataframe(dados_totais, filtro)
 
-    nome_arquivo = 'tabela-produtos'
-    salvar_dataframe_excel(df, nome_arquivo)
+    
+    df.to_json('tabela-produtos', orient='records', indent=4)
 
 if __name__ == "__main__":
     main()
--- utils/manipulacao.py
+++ utils/manipulacao.py
@@ -7,6 +7,3 @@ def criar_dataframe(resultado, filtro):
     
     return dfFiltrado
 
-def salvar_dataframe_excel(df, nome_arquivo):
-    df.to_excel(nome_arquivo + '.xlsx', index=False, engine='xlsxwriter')
-    print(f'DataFrame salvo como {nome_arquivo}.xlsx')
